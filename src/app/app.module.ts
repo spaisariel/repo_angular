245db43ba44fc766ed33278bd8323fd526603e64
spaisariel <spaisariel@gmail.com>
2020-11-05 12:04:53 -0300

    5112020 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,34 +4,36 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './components/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { HomeMsjComponent } from './components/home.msj/home.msj.component';
 import { ShortcutsComponent } from './components/shortcuts/shortcuts.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent, HomeComponent, CardComponent, HomeMsjComponent, ShortcutsComponent
+    NavigationComponent, 
+    HomeComponent, 
+    CardComponent, 
+    HomeMsjComponent, 
+    ShortcutsComponent
   ],
   imports: [MaterialModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule],
+    FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+//flex layout
+// npm uninstall @angular/flex-layout
+// ng update @angular/material
+// npm install @angular/flex-layout
+//npm i -s @angular/flex-layout @angular/cdk
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,27 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 
 
+
 const material =[
-  BrowserAnimationsModule,
   MatSliderModule,
   MatToolbarModule,
   MatButtonModule,
   MatSidenavModule,
   MatIconModule,
-  FlexLayoutModule,
   MatListModule,
   MatGridListModule,
-  MatCardModule];
+  MatCardModule,
+   ];
 
 @NgModule({
   exports: [material],
