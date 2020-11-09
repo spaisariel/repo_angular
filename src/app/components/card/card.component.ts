79fba0b49db4f79797b0983bd44dca750fa39bcc
lserroels <lserroels@gmail.com>
2020-11-09 08:43:52 -0300

    09112020 0843

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Modelo } from 'src/app/models/modeloDeEjemplo';
+import { Modelo, EntidadAcreedora } from 'src/app/models/modeloDeEjemplo';
 import { PruebaHttpService } from 'src/app/prueba-http.service';
 @Component({
   selector: 'app-card',
@@ -10,10 +10,23 @@ export class CardComponent implements OnInit {
 
   constructor(private pruebaHttpServ: PruebaHttpService ) { }
   modelo: Modelo[];
+  entidadesAcreedoras: EntidadAcreedora[];
+
+
   ngOnInit(): void {
     this.traerInfoDesdeElServicio();
+    this.entidadesAcreedoras = [
+    {nombre: 'Creditia',
+     descripcion: '01/04/2020 - $5500'},
+    {nombre: 'Galicia',
+    descripcion: '12/08/2019 - $8000'},
+    {nombre: 'Credipaz',
+    descripcion: '22/08/2018 - $8000'},
+    {nombre: 'Argenpesos',
+    descripcion: '12/12/2019 - $5400'},
+  ];
   }
-  
+
   traerInfoDesdeElServicio(): void{
   this.pruebaHttpServ.getModelosDeEjemplo().subscribe(data => this.modelo = data);
   }
