4942df8fb19916757904f68409a4ac3646d8d715
spaisariel <spaisariel@gmail.com>
2020-11-11 12:17:53 -0300

    11112020 1200 ariel

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import { Component, Input} from '@angular/core';
-import { EntidadAcreedora } from 'src/app/models/modeloDeEjemplo';
-=======
 import { Component, Input, OnInit } from '@angular/core';
 import { EntidadAcreedora } from '@models/modeloDeEjemplo';
->>>>>>> 65ba2e671ffe0d7ec6e83bfbedb28d9642ef891a
 
 @Component({
   selector: 'app-card',
@@ -12,14 +7,8 @@ import { EntidadAcreedora } from '@models/modeloDeEjemplo';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent{
-<<<<<<< HEAD
-  
-  @Input() card:EntidadAcreedora;
-  
-=======
 
   @Input() card: EntidadAcreedora;
 
->>>>>>> 65ba2e671ffe0d7ec6e83bfbedb28d9642ef891a
 
 }
